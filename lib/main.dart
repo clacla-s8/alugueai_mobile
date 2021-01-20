f83eb926a53c2b233e5b7849509e3f6f1acae0cc
clacla-s8 <clariceks8@gmail.com>
2021-01-19 22:29:28 -0300

    adicionada tela splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
-import 'package:flutter/material.dart';
 import 'package:alugueai_mobile/pages/login.page.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Alugue Aí',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.lightBlue,
-      ),
-      home: LoginPage(),
-    );
+        title: 'Alugue Aí',
+        debugShowCheckedModeBanner: false,
+        home: AnimatedSplashScreen(
+            duration: 2000,
+            splash: Image.asset('assets/logo.png'),
+            nextScreen: LoginPage(),
+            splashTransition: SplashTransition.fadeTransition,
+            pageTransitionType: PageTransitionType.scale,
+            backgroundColor: Colors.white));
   }
 }
